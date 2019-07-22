f60d723374afee4460a0eb2e6ea497163fec3bc9
Alexey Ozerov <alexozerov@gmail.com>
2019-07-22 12:43:53 +0300

    Added methods numberOfRowsInSection and cellForRowAt

--- P001_StormViewer/P001_StormViewer/P001_StormViewer/ViewController.swift
+++ P001_StormViewer/P001_StormViewer/P001_StormViewer/ViewController.swift
@@ -25,6 +25,17 @@ class ViewController: UITableViewController {
         }
     }
 
+    // MARK: - TableView Behaviours
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
 }
 
