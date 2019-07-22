aeb186821b918b9a0b9fb92ba106e2e4920790b3
Alexey Ozerov <alexozerov@gmail.com>
2019-07-22 13:45:46 +0300

    Added DetailView with ImageView inside

---
+++ P001_StormViewer/P001_StormViewer/P001_StormViewer/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  P001_StormViewer
+//
+//  Created by Alexey Ozerov on 22/07/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
