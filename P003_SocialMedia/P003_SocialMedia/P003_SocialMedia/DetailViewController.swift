bfed8c3a939a1aaf29c4915f1f7943837ae6451a
Alexey Ozerov <alexozerov@gmail.com>
2019-07-24 10:29:10 +0300

    Added "Share" button to NavBar with appropriate handler

--- P003_SocialMedia/P003_SocialMedia/P003_SocialMedia/DetailViewController.swift
+++ P003_SocialMedia/P003_SocialMedia/P003_SocialMedia/DetailViewController.swift
@@ -22,6 +22,11 @@ class DetailViewController: UIViewController {
             title = "Image \(selectedPictureNumber) of \(totalPictures)"
         }
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(shareTapped)
+        )
 
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
@@ -38,6 +43,15 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
+    @objc func shareTapped () {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 
     /*
     // MARK: - Navigation
