be228776735e261f7e7c57a341e6da182f87cdf8
Alexey Ozerov <alexozerov@gmail.com>
2019-07-29 16:10:20 +0300

    Created hardcoded URL, requested and displayed web page

--- P004_EasyBrowser/P004_EasyBrowser/ViewController.swift
+++ P004_EasyBrowser/P004_EasyBrowser/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
