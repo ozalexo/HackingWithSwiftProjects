2f87e1113f6ca7e98d866e5033b4675c167f8f79
Alexey Ozerov <alexozerov@gmail.com>
2019-07-29 15:28:19 +0300

    Created new Single View app

---
+++ P004_EasyBrowser/P004_EasyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P004_EasyBrowser
+//
+//  Created by Alexey Ozerov on 29/07/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
