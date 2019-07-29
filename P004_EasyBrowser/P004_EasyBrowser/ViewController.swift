bd49849714626128d17a268504f7d1654d67dc37
Alexey Ozerov <alexozerov@gmail.com>
2019-07-29 16:56:04 +0300

    Added page selector via UIAlertController and UIAlertAction

--- P004_EasyBrowser/P004_EasyBrowser/ViewController.swift
+++ P004_EasyBrowser/P004_EasyBrowser/ViewController.swift
@@ -17,16 +17,38 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
         view = webView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.hackingwithswift.com")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Open",
+            style: .plain,
+            target: self,
+            action: #selector(openTapped)
+        )
+
     }
 
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        // "Cancel"button doesn’t provide a handler parameter,
+        // which means iOS will just dismiss the alert controller if it’s tapped.
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+
+        present(ac, animated: true)
+    }
+
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
 
 }
 
