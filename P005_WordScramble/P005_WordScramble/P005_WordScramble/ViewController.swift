2b5febb0474307fa8932adcd6b684f474e9d59a6
Alexey Ozerov <alexozerov@gmail.com>
2019-07-31 18:52:11 +0300

    Created new Single View app

---
+++ P005_WordScramble/P005_WordScramble/P005_WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P005_WordScramble
+//
+//  Created by Alexey Ozerov on 31/07/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
