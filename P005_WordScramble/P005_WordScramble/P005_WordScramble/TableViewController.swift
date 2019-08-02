9349922955f5dcfa0d624250f261172ce2d9df30
Alexey Ozerov <alexozerov@gmail.com>
2019-08-02 16:52:33 +0300

    Added functional: pick a word via alert with closure

--- P005_WordScramble/P005_WordScramble/P005_WordScramble/TableViewController.swift
+++ P005_WordScramble/P005_WordScramble/P005_WordScramble/TableViewController.swift
@@ -16,6 +16,8 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -33,6 +35,40 @@ class TableViewController: UITableViewController {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+    }
+
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        // Here we have handler as closure
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ /*action: UIAlertAction*/ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+
+        ac.addAction(submitAction)
+
+        // For the iPads: centered alert popup
+        ac.popoverPresentationController?.sourceView = self.view
+        ac.popoverPresentationController?.sourceRect = self.view.frame
+
+        present(ac, animated: true)
+    }
+
+    func submit(_ answer: String) {
+
+    }
+
+    // MARK: - Table data
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 
 }
