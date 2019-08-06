d7fe408f0f7e476733cefaddf946af7dd4cda27d
Alexey Ozerov <alexozerov@gmail.com>
2019-08-07 01:14:38 +0300

    Added five UILabels by code
    Disabled `translatesAutoresizingMaskIntoConstraints` property for all labels.

--- P006B_AutoLayoutInCode/P006B_AutoLayoutInCode/ViewController.swift
+++ P006B_AutoLayoutInCode/P006B_AutoLayoutInCode/ViewController.swift
@@ -12,7 +12,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let label1 = UILabel()
+
+        /**
+        By default iOS generates Auto Layout constraints for you based on a view's size and position.
+        We'll be doing it by hand, so we need to disable this feature.
+        */
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        // Label will be sized to its content
+        label1.sizeToFit()
+
+        let label2 = UILabel()
+
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
     }
 
 
