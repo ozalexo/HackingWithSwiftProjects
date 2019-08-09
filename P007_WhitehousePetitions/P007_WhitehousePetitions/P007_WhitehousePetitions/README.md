5a6fb6aef16e1d86734b8f8f6ba877723fefd7ed
Alexey Ozerov <alexozerov@gmail.com>
2019-08-10 01:04:25 +0300

    Created new Single View app

---
+++ P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/README.md
@@ -0,0 +1,2 @@
+#  Project 007 "Whitehouse Petitions"
+
---
+++ P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P007_WhitehousePetitions
+//
+//  Created by Alexey Ozerov on 10/08/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
