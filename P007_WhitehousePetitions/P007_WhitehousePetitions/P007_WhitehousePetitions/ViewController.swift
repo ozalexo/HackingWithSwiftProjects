c6e6c5279df0c39d6791a16941cf69f984ab8979
Alexey Ozerov <alexozerov@gmail.com>
2019-08-10 02:59:07 +0300

    Added "Top Rated" TabBar Item (via code)

--- P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/ViewController.swift
+++ P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
 
         // Bad idea to download data in main thread in viewDidLoad. UI will be stalled during download
         if let url = URL(string: urlString) {
