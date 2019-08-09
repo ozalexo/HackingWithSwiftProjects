9e387f050976df283e09a74e3447289e1d673eef
Alexey Ozerov <alexozerov@gmail.com>
2019-08-10 02:26:39 +0300

    Added JSON parser

--- P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/ViewController.swift
+++ P007_WhitehousePetitions/P007_WhitehousePetitions/P007_WhitehousePetitions/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+
+        // Bad idea to download data in main thread in viewDidLoad. UI will be stalled during download
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
