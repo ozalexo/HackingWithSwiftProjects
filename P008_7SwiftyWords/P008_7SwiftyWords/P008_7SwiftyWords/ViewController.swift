aa28c45974c5d871961022c9d1046e0ed349244e
Alexey Ozerov <alexozerov@gmail.com>
2019-08-21 10:51:47 +0300

    Label for scores. With manual constraints.

--- P008_7SwiftyWords/P008_7SwiftyWords/P008_7SwiftyWords/ViewController.swift
+++ P008_7SwiftyWords/P008_7SwiftyWords/P008_7SwiftyWords/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
 
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
