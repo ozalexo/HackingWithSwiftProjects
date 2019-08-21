118f437895c8298c359726f1e0c8e7c15c024a72
Alexey Ozerov <alexozerov@gmail.com>
2019-08-21 14:55:23 +0300

    Handlers: clearTapped and letterTapped

--- P008_7SwiftyWords/P008_7SwiftyWords/P008_7SwiftyWords/ViewController.swift
+++ P008_7SwiftyWords/P008_7SwiftyWords/P008_7SwiftyWords/ViewController.swift
@@ -181,13 +181,25 @@ class ViewController: UIViewController {
         }
     }
 
+    // MARK: - Handlers
     @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
 
     @objc func submitTapped(_ sender: UIButton) {
     }
 
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+
+        activatedButtons.removeAll()
     }
 
     // MARK: - Loading levels
